b330c90a73071ac4bb17e212e59923a40b77836b
fulsun <54365873+fulsun@users.noreply.github.com>
2025-06-06 20:21:37 +0800

    refactor(ui): 合并静态路由配置到单独文件

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,14 +1,5 @@
 import {createRouter, createWebHistory} from 'vue-router'
-
-const routes = [
-
-    // 404 页面 - 必须放在最后
-    {
-        path: '/:pathMatch(.*)*',
-        name: 'NotFound',
-        component: () => import('@/views/sys/error-page/404.vue')
-    }
-]
+import routes from './routes'
 
 const router = createRouter({
     history: createWebHistory(),   // 我们使用该模式可以去除 链接里边的#
---
+++ frontend/src/router/routes.js
@@ -0,0 +1,33 @@
+export const Layout = () => import("@/layout/index.vue");
+
+// 静态路由配置
+const constantRoutes = [
+    {
+        path: "/",
+        name: "Layout",
+        component: Layout,
+        redirect: "/dashboard",
+        children: [
+            {
+                path: "dashboard",
+                name: "Dashboard",
+                component: () => import("@/views/dashboard/index.vue"),
+                meta: {
+                    title: "工作台",
+                    icon: "HomeFilled",
+                }
+            }
+        ],
+
+    },
+
+    // 404 页面 - 必须放在最后
+    {
+        path: '/:pathMatch(.*)*',
+        name: 'NotFound',
+        component: () => import('@/views/sys/error-page/404.vue')
+    }
+];
+export default [
+    ...constantRoutes
+]
