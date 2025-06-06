4b51a25fd2bef2b153f30bfaf583b85413ea9779
fulsun <54365873+fulsun@users.noreply.github.com>
2025-06-06 20:42:36 +0800

    feat: 添加ElementPlus图标全局注册

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,14 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from '@/router'; // 导入路由配置
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 const app = createApp(App);
+// 全局注册所有图标
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
+
 app.use(router); // 挂载路由
 app.mount('#app');
