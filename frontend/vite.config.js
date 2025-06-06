ade64fa399fb49ac4ef97ac295290c0d57f915ea
fulsun <54365873+fulsun@users.noreply.github.com>
2025-06-06 16:58:14 +0800

    feat: 添加 Element Plus 及相关依赖并配置按需导入

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,24 @@
-import {defineConfig} from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vite.dev/config/
 export default defineConfig({
     build: {
         outDir: path.resolve(__dirname, '../backend/src/main/resources/static/')
     },
-    plugins: [vue()],
+    plugins: [
+        vue(),
+        AutoImport({
+            resolvers: [ElementPlusResolver()],
+        }),
+        Components({
+            resolvers: [ElementPlusResolver()],
+        }),
+    ],
     devServer: {
         port: 3000, // 设置端口号
         // host: '0.0.0.0', // 允许外部访问
@@ -18,7 +29,7 @@ export default defineConfig({
             '/api': {
                 target: 'http://127.0.0.1:8080', // 目标服务器地址
                 changeOrigin: true, // 允许跨域
-                pathRewrite: {'^/api': ''} // 重写路径
+                pathRewrite: { '^/api': '' } // 重写路径
             }
         }
     },
