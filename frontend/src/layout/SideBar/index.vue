7c89aac0869f0292db2be14d1e58c62de30c5fa3
fulsun <54365873+fulsun@users.noreply.github.com>
2025-06-07 08:51:04 +0800

    feat(core): 新增侧边栏顶部

---
+++ frontend/src/layout/SideBar/index.vue
@@ -0,0 +1,80 @@
+<template>
+  <!-- 侧边栏菜单 -->
+  <div class="side-bar-horizontal">
+    <!-- 侧边栏顶部logo -->
+    <div class="side-bar-header">
+      <div class="side-bar-logo">
+        <img alt="logo" src="@/assets/img/logo.png"> </img>
+        <span>Hexo Admin</span>
+      </div>
+
+
+    </div>
+
+  </div>
+
+</template>
+
+<script setup>
+import {ref} from 'vue'
+
+const props = defineProps({
+  mode: {
+    type: String,
+    default: 'vertical'
+  }
+})
+const sidebar = ref({
+  opened: true
+});
+
+const toggleSideBar = () => {
+  console.log('toggleSideBar')
+  sidebar.value.opened = !sidebar.value.opened // 注意这里有两个修改
+}
+// 初始化菜单
+const filteredRoutes = ref([])
+
+const initSideMenus = async () => {
+  const menusStore = useMenuStore()
+  filteredRoutes.value = await menusStore.generateMenus()
+}
+
+</script>
+
+<style lang="scss" scoped>
+@use "@/styles/variables.scss" as *;
+
+
+.side-bar-horizontal {
+  width: $sidebar-width;
+  background-color: #fff;
+  border-right: 1px solid var(--el-border-color);
+  border-radius-left: 10px;
+
+  .side-bar-header {
+    height: $side-bar-header-height;
+    border-bottom: 1px solid var(--el-border-color);
+
+    .side-bar-logo {
+      display: flex;
+      align-items: center; // 垂直居中
+      justify-content: center; // 水平居中
+
+      img {
+        width: 75px;
+        height: $side-bar-header-height;
+      }
+
+      span {
+        font-size: 20px;
+        color: inherit !important;
+      }
+    }
+
+  }
+
+}
+
+
+</style>
---
+++ frontend/src/layout/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="main-layout">
+    <!-- 侧边栏-->
+    <SideBar/>
+  </div>
+</template>
+
+<script setup>
+import SideBar from '@/layout/SideBar/index.vue';
+</script>
+
+<style lang="scss" scoped>
+.main-layout {
+  width: 100%;
+  height: 100%;
+  box-sizing: border-box; // width 和 height 包含 padding 和 border，
+
+}
+</style>
