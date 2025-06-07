98d08a66b8e130da597789106fae61e590f8b64f
fulsun <54365873+fulsun@users.noreply.github.com>
2025-06-08 00:45:44 +0800

    refactor(ui): 优化layout Head结构并调整样式

--- frontend/src/layout/index.vue
+++ frontend/src/layout/index.vue
@@ -1,61 +1,29 @@
 <template>
   <el-container class="layout column">
-    <el-aside class="left-layout">
-      <SideBar/>
-    </el-aside>
-    <el-container class="right-layout">
-      <el-header class="header-layout">
-        <Header/>
-      </el-header>
-      <el-main class="main-layout">Main</el-main>
-      <el-footer class="footer-layout">
-        <SideBarFooter/>
-      </el-footer>
+    <el-header class="header-layout">
+      <span>Hexo Admin</span>
+      <Header/>
+    </el-header>
+    <el-container class="column-inner">
+      <el-aside :width="isCollapse ? '65px' : '200px'">
+        <SideBar/>
+      </el-aside>
+      <el-container>
+        <el-main>Main</el-main>
+        <el-footer>
+          footer
+        </el-footer>
+      </el-container>
     </el-container>
   </el-container>
 </template>
 
 <script setup>
 import SideBar from '@/layout/SideBar/index.vue';
-import SideBarFooter from "@/layout/SideBar/components/SideBarFooter.vue";
 import Header from "@/layout/components/Header/index.vue";
 </script>
 
 <style lang="scss" scoped>
-@use "@/styles/variables.scss" as *;
-
-.common-layout {
-  height: 100vh;
-  width: 100%;
-  box-sizing: border-box; // width 和 height 包含 padding 和 border，
-}
-
-/* 左侧边栏 */
-.left-layout {
-  width: $sidebar-width;
-  overflow-x: hidden;
-}
-
-/* 右侧内容 */
-.right-layout {
-  width: auto;
-  height: 100vh;
-}
-
-.header-layout {
-  height: $side-bar-header-height;
-  padding: 0;
-  overflow: hidden;
-}
-
-.main-layout {
-  height: 900px;
-}
-
-.footer-layout {
-  height: 100px;
-  padding: 0;
-  overflow: hidden;
-}
+@use "index.scss";
 
 </style>
