9e255a5a83802daf41b5ef284097902117b69fdd
fulsun <54365873+fulsun@users.noreply.github.com>
2025-06-09 21:12:30 +0800

    feat: 新增静态资源处理配置

---
+++ backend/src/main/java/pers/fulsun/hexoadmin/config/SapWebConfig.java
@@ -0,0 +1,35 @@
+package pers.fulsun.hexoadmin.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+
+import java.io.IOException;
+
+@Configuration
+public class SapWebConfig implements WebMvcConfigurer {
+
+    /**
+     * 配置静态资源映射 确保所有前端路由的请求都返回index.html
+     *
+     * @param registry
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/")
+                .resourceChain(true) // 启用资源链路处理
+                .addResolver(new PathResourceResolver() {
+                    @Override
+                    protected Resource getResource(String resourcePath, Resource location) throws IOException {
+                        Resource requestedResource = location.createRelative(resourcePath);
+                        return requestedResource.exists() && requestedResource.isReadable()
+                                ? requestedResource
+                                : new ClassPathResource("/static/index.html");
+                    }
+                });
+
+    }
+}
