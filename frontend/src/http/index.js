bf69e0fe9dc44e4301301780c1275b471115b2f5
fulsun <54365873+fulsun@users.noreply.github.com>
2025-06-09 23:52:55 +0800

    fix(frontend): 添加默认请求头、优化错误处理逻辑

--- frontend/src/http/index.js
+++ frontend/src/http/index.js
@@ -8,7 +8,9 @@ const config = {
     // 默认地址请求地址
     baseURL: import.meta.env.VITE_API_BASE_URL, // 设置超时时间 单位毫秒
     timeout: 30000, // 跨域时候允许携带凭证
-    withCredentials: true,
+    headers: {
+        'Content-Type': 'application/json', // 强制使用 JSON
+    }, withCredentials: true,
 };
 
 const service = axios.create({
@@ -51,13 +53,18 @@ service.interceptors.response.use(response => {
         console.log('repeated request: ' + error.message)
     } else {
         const {response} = error;
-        // 请求超时 && 网络错误单独判断，没有 response
-        if (error.message.indexOf("timeout") !== -1) ElMessage.error("请求超时！请您稍后重试");
-        if (error.message.indexOf("Network Error") !== -1) ElMessage.error("网络错误！请您稍后重试");
-        // 根据响应的错误状态码，做不同的处理
-        if (response) checkStatus(response.status);
-        // 服务器结果都没有返回(可能服务器错误可能客户端断网)，断网处理:可以跳转到断网页面
-        if (!window.navigator.onLine) router.replace("/500");
+        // 尝试获取后端返回的错误信息
+        if (response && response.data && response.data.error) {
+            ElMessage.error(response.data.error);
+        } else {
+            // 请求超时 && 网络错误单独判断，没有 response
+            if (error.message.indexOf("timeout") !== -1) ElMessage.error("请求超时！请您稍后重试");
+            if (error.message.indexOf("Network Error") !== -1) ElMessage.error("网络错误！请您稍后重试");
+            // 根据响应的错误状态码，做不同的处理
+            if (response) checkStatus(response.status);
+            // 服务器结果都没有返回(可能服务器错误可能客户端断网)，断网处理:可以跳转到断网页面
+            if (!window.navigator.onLine) router.replace("/500");
+        }
     }
     return Promise.reject(error);
 });
