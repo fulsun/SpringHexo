4d60dbabf08e7a7464e88396dd0e69c84ea4ed96
fulsun <54365873+fulsun@users.noreply.github.com>
2025-06-10 07:47:40 +0800

    refactor(scripts): 根据模式配置mock服务和代理路径

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,73 +11,55 @@ import IconsResolver from "unplugin-icons/resolver";
 
 
 // https://vite.dev/config/
-export default defineConfig({
-    build: {
-        outDir: path.resolve(__dirname, 'dist')
-    },
-    plugins: [
-        vue(),
-        AutoImport({
-            resolvers: [
-                // 配置Element Plus采用saas样式配色系统
-                ElementPlusResolver({importStyle: 'sass'}),
-            ],
-        }),
-        Components({
-            resolvers: [
-                // 自动导入 Element Plus 组件
-                ElementPlusResolver(),
-                // 自动导入 Iconify 图标
-                IconsResolver()
-            ],
-        }),
-        createSvgIconsPlugin({
+export default defineConfig(({mode}) => {
+    console.log('当前运行模式:', mode); // 添加调试日志
+    return {
+        build: {
+            outDir: path.resolve(__dirname, '../backend/src/main/resources/static/')
+        }, plugins: [vue(), AutoImport({
+            resolvers: [// 配置Element Plus采用saas样式配色系统
+                ElementPlusResolver({importStyle: 'sass'}),],
+        }), Components({
+            resolvers: [// 自动导入 Element Plus 组件
+                ElementPlusResolver(), // 自动导入 Iconify 图标
+                IconsResolver()],
+        }), createSvgIconsPlugin({
             // 指定图标文件夹
-            iconDirs: [path.resolve(__dirname, './src/assets/svgs')],
-            // 指定symbolId格式
-            symbolId: 'icon-[dir]-[name]',
-            svgoOptions: true, // 传递给svgo的配置项
-        }),
-        viteMockServe({
+            iconDirs: [path.resolve(__dirname, './src/assets/svgs')], // 指定symbolId格式
+            symbolId: 'icon-[dir]-[name]', svgoOptions: true, // 传递给svgo的配置项
+        }), viteMockServe({
             mockPath: 'mock', // mock文件存放目录
-            localEnabled: true, // 开发环境启用
+            localEnabled: mode === 'mock', // 根据模式决定是否启用mock
             prodEnabled: false, // 生产环境禁用
             logger: true, // 启用日志，方便调试
-        }),
-        Icons({
-            compiler: 'vue3',
-            autoInstall: true, // 自动安装图标集（按需）
+        }), Icons({
+            compiler: 'vue3', autoInstall: true, // 自动安装图标集（按需）
             // 关键配置：启用运行时图标加载
-            runtimeCompiler: true,
-            scale: 1, // 缩放比例
+            runtimeCompiler: true, scale: 1, // 缩放比例
             defaultClass: 'iconify', // 默认类名
-        })
-    ],
-    css: {
-        preprocessorOptions: {
-            scss: {
-                // 自动导入定制化样式文件进行样式覆盖
-                additionalData: `@use "@/styles/element/index.scss" as *;`
+        })], css: {
+            preprocessorOptions: {
+                scss: {
+                    // 自动导入定制化样式文件进行样式覆盖
+                    additionalData: `@use "@/styles/element/index.scss" as *;`
+                }
             }
-        }
-    },
-    devServer: {
-        port: 3000, // 设置端口号
-        // host: '0.0.0.0', // 允许外部访问
-        // https: false, // 不启用HTTPS
-        // open: true, // 启动时自动打开浏览器
-        // hot: true, // 热更新
-        proxy: {
-            '/api2': {
-                target: 'http://127.0.0.1:8080', // 目标服务器地址
-                changeOrigin: true, // 允许跨域
-                pathRewrite: {'^/api': ''} // 重写路径
+        }, server: {
+            port: 3000, // 设置端口号
+            // host: '0.0.0.0', // 允许外部访问
+            // https: false, // 不启用HTTPS
+            // open: true, // 启动时自动打开浏览器
+            // hot: true, // 热更新
+            proxy: {
+                '/api': {
+                    target: 'http://127.0.0.1:8080', // 目标服务器地址
+                    changeOrigin: true, // 允许跨域
+                }
             }
-        }
-    },
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './src'),
+        }, resolve: {
+            alias: {
+                '@': path.resolve(__dirname, './src'),
+            }
         }
     }
-})
+});
